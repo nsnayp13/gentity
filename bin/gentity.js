b67d84ca7708411af23f2df64c4671934c8047ba
Ivanov.n/work <ivanov@etsgroup.ru>
2020-11-13 12:43:27 +0300

    fix: test

--- bin/gentity.js
+++ bin/gentity.js
@@ -1,13 +1,4 @@
 #!/usr/bin/env node
-import { Cli, AstMorph } from '../src/index.js';
-
- const ast = new AstMorph('Tester');
- ast.createOrModifyClass();
-ast.comImport('Column');
-ast.comImport('Column')
-ast.comImport('Entity')
-    ast.save()
-
-
-// const cli = new Cli()
-// cli.start();
+import { Cli } from '../src/index.js';
+const cli = new Cli()
+cli.start();
